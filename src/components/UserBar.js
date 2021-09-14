1c1a370ffc5170489b231d91f7458344a372fbdf
Amy <amycheck8715@gmail.com>
2021-09-14 07:53:12 -0400

    added video player

--- src/components/UserBar.js
+++ src/components/UserBar.js
@@ -1,6 +1,17 @@
+import ReactPlayer from "react-player";
+
+
 function UserBar () {
+
     return (
             <div className="userBar" >
+                <div className="playerWrapper">
+                    <ReactPlayer
+                        className='reactPlayer'
+                        url='https://www.youtube.com/watch?v=Pn-6eOxnEMI'
+                        width='max-width'
+                    />
+                </div>
             </div>
     );
 
